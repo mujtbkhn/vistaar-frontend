b5570162a4ddd5829da3ef230c1d48e2d3c3eb53
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-09-27 17:43:15 +0530

    installed axios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: ['axios'], // Add axios to the external array
+        },
+    },
+});
